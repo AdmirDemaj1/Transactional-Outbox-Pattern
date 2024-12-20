d071cc5b5fdbddb4d2cd85aacfaf231361306fbb
a.demaj <a.demaj@teamsystem.com>
2024-12-20 17:00:25 +0100

    add apache kafka and push topics on cdc

--- embeded-debezium/student-cdc-relay/src/main/java/com/sohan/student/listener/CDCListener.java
+++ embeded-debezium/student-cdc-relay/src/main/java/com/sohan/student/listener/CDCListener.java
@@ -1,158 +1,150 @@
 package com.sohan.student.listener;
 
+import lombok.Builder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.sohan.student.elasticsearch.service.StudentService;
+import com.sohan.student.elasticsearch.entity.Student;
 import com.sohan.student.utils.Operation;
 import io.debezium.config.Configuration;
-import io.debezium.embedded.EmbeddedEngine;
 import io.debezium.engine.ChangeEvent;
 import io.debezium.engine.DebeziumEngine;
 import io.debezium.engine.format.Json;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.apache.commons.lang3.tuple.Pair;
-import org.apache.kafka.connect.data.Field;
-import org.apache.kafka.connect.data.Struct;
-import org.apache.kafka.connect.source.SourceRecord;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import java.util.Set;
+import java.util.Arrays;
+import java.util.HashSet;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import com.google.gson.Gson;
 
-
-import static io.debezium.data.Envelope.FieldName.*;
-import static java.util.stream.Collectors.toMap;
-
-import java.io.IOException;
-
-/**
- * This class creates, starts and stops the EmbeddedEngine, which starts the Debezium engine. The engine also
- * loads and launches the connectors setup in the configuration.
- * <p>
- * The class uses @PostConstruct and @PreDestroy functions to perform needed operations.
- *
- * @author Sohan
- */
 @Component
+@Slf4j
 public class CDCListener {
-
-    /**
-     * Single thread pool which will run the Debezium engine asynchronously.
-     */
     private final Executor executor = Executors.newSingleThreadExecutor();
-
-
-
-    /**
-     * The Debezium engine which needs to be loaded with the configurations, Started and Stopped - for the
-     * CDC to work.
-     */
     private final DebeziumEngine<ChangeEvent<String, String>> engine;
-
-    /**
-     * Handle to the Service layer, which interacts with ElasticSearch.
-     */
     private final StudentService studentService;
-
-    
-    
-
-    /**
-     * Constructor which loads the configurations and sets a callback method 'handleEvent', which is invoked when
-     * a DataBase transactional operation is performed.
-     *
-     * @param studentConnector
-     * @param studentService
-     * @throws IOException 
-     */
-    private CDCListener(Configuration studentConnector, StudentService studentService) throws IOException {
-        // Create the engine with this configuration ...
-        try (DebeziumEngine<ChangeEvent<String, String>> engine = DebeziumEngine.create(Json.class)
-                .using(studentConnector.asProperties())
-                .notifying(record -> {
-                    log.info("bla bla blaaaa");
-                    System.out.println(record);
-                }).build()
-            ) {
-
-                this.engine = engine;
-
-                // Run the engine asynchronously ...
-                ExecutorService executor = Executors.newSingleThreadExecutor();
-                executor.execute(engine);
-
-                // Do something else or wait for a signal or an event
-              
-                // Engine is stopped when the main code is finished
-        }
-
-        this.studentService = studentService;
-    }
+    private final KafkaProducer<String, String> producer;
 
     private static final Logger log = LoggerFactory.getLogger(CDCListener.class);
+    
+    @Value("${kafka.topic.student}")
+    private String studentTopic;
 
-    /**
-     * The method is called after the Debezium engine is initialized and started asynchronously using the Executor.
-     */
+    @Autowired
+    public CDCListener(Configuration studentConnector, 
+                      StudentService studentService,
+                      KafkaProducer<String, String> kafkaProducer) throws IOException {
+        this.studentService = studentService;
+        this.producer = kafkaProducer;
+        
+        this.engine = DebeziumEngine.create(Json.class)
+            .using(studentConnector.asProperties())
+            .notifying(this::handleEvent)
+            .build();
+    }
+
+private void handleEvent(ChangeEvent<String, String> event) {
+        String value = event.value();
+        if (value != null) {
+            try {
+                log.info("Received event: {}", value);
+                JsonObject jsonObject = JsonParser.parseString(value).getAsJsonObject();
+                JsonElement payload = jsonObject.get("payload");
+                
+                if (payload != null && payload.isJsonObject()) {
+                    JsonObject payloadObject = payload.getAsJsonObject();
+                    JsonElement opElement = payloadObject.get("op");
+                    
+                    if (opElement != null) {
+                        String operation = opElement.getAsString();
+                        
+                        if ("c".equals(operation)) {
+                            JsonElement afterElement = payloadObject.get("after");
+                            if (afterElement != null && afterElement.isJsonObject()) {
+                                JsonObject afterObject = afterElement.getAsJsonObject();
+                                Student student = new Student();
+                                
+                                if (afterObject.has("id")) {
+                                    student.setId(afterObject.get("id").getAsInt());
+                                }
+                                if (afterObject.has("name")) {
+                                    student.setName(afterObject.get("name").getAsString());
+                                }
+                                if (afterObject.has("address")) {
+                                    student.setAddress(afterObject.get("address").getAsString());
+                                }
+                                if (afterObject.has("email")) {
+                                    student.setEmail(afterObject.get("email").getAsString());
+                                }
+
+                                ProducerRecord<String, String> record = 
+                                    new ProducerRecord<>(studentTopic, new Gson().toJson(student));
+                                
+                                producer.send(record, (metadata, exception) -> {
+                                    if (exception == null) {
+                                        log.info("Message sent successfully to partition {}", 
+                                            metadata.partition());
+                                    } else {
+                                        log.error("Failed to send message", exception);
+                                    }
+                                });
+                            }
+                        }
+                        
+                        Map<String, Object> studentMap = extractStudentData(payloadObject);
+                        studentService.maintainReadModel(studentMap, Operation.forCode(operation));
+                    }
+                }
+            } catch (Exception e) {
+                log.error("Error processing event", e);
+            }
+        }
+    }
+
+private Map<String, Object> extractStudentData(JsonObject payload) {
+    Map<String, Object> studentMap = new HashMap<>();
+    if (payload.has("after")) {
+        JsonObject after = payload.getAsJsonObject("after");
+        if (after.has("id")) studentMap.put("id", after.get("id").getAsInt());
+        if (after.has("name")) studentMap.put("name", after.get("name").getAsString());
+        if (after.has("address")) studentMap.put("address", after.get("address").getAsString());
+        if (after.has("email")) studentMap.put("email", after.get("email").getAsString());
+    }
+    return studentMap;
+}
+
     @PostConstruct
     private void start() {
         this.executor.execute(engine);
     }
 
-    /**
-     * This method is called when the container is being destroyed. This stops the debezium, merging the Executor.
-     */
     @PreDestroy
     private void stop() {
-        if (this.engine != null) {
+        if (producer != null) {
+            producer.close();
+        }
+        if (engine != null) {
             try {
-                this.engine.close();
+                engine.close();
             } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
+                log.error("Error closing engine", e);
             }
         }
     }
-
-    /**
-     * This method is invoked when a transactional action is performed on any of the tables that were configured.
-     *
-     * @param sourceRecord
-     */
-   private void handleEvent(SourceRecord sourceRecord) {
-    Struct sourceRecordValue = (Struct) sourceRecord.value();
-
-    if (sourceRecordValue != null) {
-        log.info("Received record: {}", sourceRecordValue);
-        Operation operation = Operation.forCode((String) sourceRecordValue.get(OPERATION));
-        
-        if (operation != Operation.READ) {
-            log.info("Operation: {}", operation);
-
-            Map<String, Object> message;
-            String record = AFTER; // For Update & Insert operations.
-
-            if (operation == Operation.DELETE) {
-                record = BEFORE; // For Delete operations.
-            }
-
-            Struct struct = (Struct) sourceRecordValue.get(record);
-            message = struct.schema().fields().stream()
-                    .map(Field::name)
-                    .filter(fieldName -> struct.get(fieldName) != null)
-                    .map(fieldName -> Pair.of(fieldName, struct.get(fieldName)))
-                    .collect(toMap(Pair::getKey, Pair::getValue));
-
-            log.info("Message data: {}", message);
-            this.studentService.maintainReadModel(message, operation);
-            log.info("Data Changed: {} with Operation: {}", message, operation.name());
-        }
-    }
-}
-
 }
